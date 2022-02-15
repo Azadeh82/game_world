7e3b0d2cba30448477ba06bc9a7c634c8ef0fb4f
Azadeh <az.gh558@gmail.com>
2022-02-15 11:02:50 +0100

    terminer le style et ajouter les page de Ben au navbar

--- app/Http/Controllers/CommandeController.php
+++ app/Http/Controllers/CommandeController.php
@@ -7,25 +7,6 @@ use Illuminate\Http\Request;
 
 class CommandeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -50,37 +31,4 @@ class CommandeController extends Controller
         return view('commande.show' , compact('commande'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
